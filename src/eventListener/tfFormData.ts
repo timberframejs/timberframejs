340f81da374c3ab7d68c14a83a5348155baf5b0a
MikeTDev <TMTenley@gmail.com>
2024-04-11 09:12:21 -0400

    Adds complex data model and replaces form data model with it in CedPost.
    Signed-off-by: Mike Tenley <ttenley@cydecor.com>
    Signed-off-by: Mike Tenley <tmtenley@gmail.com>

--- src/eventListener/tfFormData.ts
+++ src/eventListener/tfFormData.ts
@@ -110,3 +110,105 @@ export function makeFormData(node: HTMLElement, ele: tfElementType): FormData {
 
   return fd;
 }
+
+
+export function makeComplexData(node: HTMLElement, ele: tfElementType): any {
+  // is single input element so no children to query
+  const controls = []
+  if (node.tagName === 'INPUT') {
+    controls.push(node);
+  }
+  if (node.tagName === 'BUTTON') {
+    controls.push(node)
+  }
+  // todo:get all form controls
+  const inputs = node.querySelectorAll(':scope input')
+  controls.push(...inputs)// might not want to spread here, but quick and easy
+  // todo: need to do other controls here
+  const selects = getSelects(node)
+  selects.forEach(sel => {
+    controls.push(...sel)
+  })
+
+  const textAreas = node.querySelectorAll(':scope textarea')
+  controls.push(...textAreas)
+
+  let complexObject = {};
+  let parsedNames = [];
+  controls.forEach(ctrol => {   
+    let parts = ctrol.name.split('.');
+    if (parsedNames.indexOf(ctrol.name) > -1) {
+      console.warn(`Developer, you may have a copy/paste error in your form or tf-include tree. There is more than one form control with the name ${ctrol.name}`)
+    } else {
+
+      // create a complex object by breaking "." in control names
+      if(parts.length === 1) {
+
+        // if name indicates it's an array, push object value into an array.
+        if(parts[0].indexOf('[]') > -1) { 
+          ctrol.name = ctrol.name.substring(0, ctrol.name.length - 2);
+
+          // create array if it doesnt exist
+          if(complexObject[ctrol.name] == null) {
+            complexObject[ctrol.name] = [];
+          }
+          
+          complexObject[ctrol.name].push(ctrol.value);
+          
+        } else {
+
+          // simple object value assignment
+          complexObject[ctrol.name] = ctrol.value;
+        }
+        
+      } else {
+        // create parent object if it doesn't exist
+        if(complexObject[parts[0]] == null) {
+          complexObject[parts[0]] = {}
+        }
+
+        // let our tree root start at parts 0
+        let propTree = complexObject[parts[0]] 
+
+        // iterate through each part and add an object if it doesn't exist
+        for(let i = 1; i < parts.length; i++) {
+
+          if(propTree[parts[i]] == null) {
+            // for each part create a parent object if needed
+            if(i === parts.length - 1) {
+
+              // if part name indicates an array, handle it
+              if(parts[i].indexOf('[]') > -1) { 
+
+                // get part name by removing brackets
+                let propName = parts[i].substring(0, parts[i].length - 2);
+
+                // create object if needed
+                if(propTree[propName] == null) {
+                  propTree[propName] = [];
+                }
+
+                // push data into array
+                propTree[propName].push(ctrol.value);
+              } else {
+                propTree[parts[i]] = ctrol.value;
+              }
+              
+            } else {
+              propTree[parts[i]] = {};
+            }
+          }
+          propTree = propTree[parts[i]];
+        }
+      }
+    }
+  })
+
+  
+  return complexObject;
+}
+
+export function getFormDataAsType<Type>(node: HTMLElement, ele: tfElementType): Type {
+  let object = makeComplexData(node, ele);
+  return object as Type;
+}
