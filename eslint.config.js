6d342504b23c30fcbeaf8cb96f6556278bbf6358
porter2311 <Crochet0908@gmail.com>
2025-10-29 22:21:17 -0400

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], ...js.configs.recommended, languageOptions: { globals: globals.browser } },
+  { ignores: ["dist/**/*", "node_modules/**/*", "**/*.js"] },
+  { files: ["**/*.{ts,mts,cts}"], ...js.configs.recommended, languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
 ];
